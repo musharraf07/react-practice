5929e7615cfff46faabce350ee4aaf6a395f48e3
Mohammed Musharraf <121166414+musharraf07@users.noreply.github.com>
2024-11-21 18:55:38 +0530

    chapter-01

---
+++ 01_Revision/script.js
@@ -0,0 +1,34 @@
+/*
+const heading = React.createElement("h1",{className:'greet',id:'heading'},"hello world from react!");
+
+const root = ReactDOM.createRoot(document.getElementById('container'));
+
+root.render(heading);
+
+console.log(heading); // it returns objects 
+
+*/
+
+
+
+// nested HTML structure inside react
+const parent = React.createElement(
+    'div',{id:'parent'},
+    [React.createElement('div',{id:'child1'}, 
+        [React.createElement('h1',{},"I am an H1 tag" ),
+        React.createElement('h2',{},"I am an H2 tag" )
+        ]
+    ),
+    React.createElement('div',{id:'child2'}, 
+        [React.createElement('h1',{},"I am an H1 tag" ),
+        React.createElement('h2',{},"I am an H2 tag" )
+        ]
+    )]
+    
+    );
+
+    const root = ReactDOM.createRoot(document.getElementById('container'));
+    
+    root.render(parent);
+    console.log(parent)
+
